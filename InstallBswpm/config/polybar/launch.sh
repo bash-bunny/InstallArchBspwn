2fadaaf059dcbd5d371241caddcdce2710e94cf2
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-04-11 20:16:26 +0200

    Update launch.sh

--- InstallBswpm/config/polybar/launch.sh
+++ InstallBswpm/config/polybar/launch.sh
@@ -8,4 +8,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar -c ~/.config/polybar/config_top_fixed &
+polybar -c ~/.config/polybar/config &
