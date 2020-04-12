1661e4047dcb8c821af2ecea34e87aae29c75eba
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-04-12 19:06:26 +0200

    Update bspwmInstall.sh

--- InstallBswpm/bspwmInstall.sh
+++ InstallBswpm/bspwmInstall.sh
@@ -274,6 +274,7 @@ yay -S --noconfirm ttf-font-awesome
 yay -S --noconfirm ttf-mac-fonts
 yay -S --noconfirm nerd-fonts-hack 
 yay -S --noconfirm vivaldi
+yay -S --noconfirm oblogout
 
 # these come always last
 
