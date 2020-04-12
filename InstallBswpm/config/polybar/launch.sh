0262cd66d197d93b513966ad67a4cdcb97043c30
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-04-12 18:45:59 +0200

    Update launch.sh

--- InstallBswpm/config/polybar/launch.sh
+++ InstallBswpm/config/polybar/launch.sh
@@ -10,7 +10,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 if type "xrandr" > /dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-bspwm -c $/.config/polybar/config &
+    MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
   done
 else
   polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
