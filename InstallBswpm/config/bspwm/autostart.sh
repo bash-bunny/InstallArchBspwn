4d4744c5a5ad9c02147a850839996974236547ce
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-04-19 01:29:13 +0200

    Update autostart.sh

--- InstallBswpm/config/bspwm/autostart.sh
+++ InstallBswpm/config/bspwm/autostart.sh
@@ -34,6 +34,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
+unclutter &		# Remove mouse when idle
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
