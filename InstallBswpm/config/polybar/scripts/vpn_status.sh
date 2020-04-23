9dd6383b61c96c65ca763af240edcdd3a8d967b4
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-04-23 22:24:19 +0200

    Update vpn_status.sh

--- InstallBswpm/config/polybar/scripts/vpn_status.sh
+++ InstallBswpm/config/polybar/scripts/vpn_status.sh
@@ -4,7 +4,7 @@ IFACE=$(/usr/bin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 
 if [ "$IFACE" = "tun0" ]
 then
-    echo "%{F#11d929} %{F#e2ee6a}$(/usr/bin/ifconfig tun0 | grep "inet " | awk '{print $2}')"
+    echo "%{F#49088a} %{F#e2ee6a}$(/usr/bin/ifconfig tun0 | grep "inet " | awk '{print $2}')"
 else
-    echo "%{F#bad6be} %{F#e2ee6a}"
+    echo "%{F#d0bfe0} %{F#e2ee6a}"
 fi
