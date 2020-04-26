f2b9ea32c146c60b5a351ec7ba3f15d3c2b3049e
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-04-26 17:57:52 +0200

    Update bspwmInstall.sh

--- InstallBswpm/bspwmInstall.sh
+++ InstallBswpm/bspwmInstall.sh
@@ -313,7 +313,6 @@ sudo pacman -S gtop --noconfirm --needed
 sudo pacman -S imagemagick --noconfirm --needed
 sudo pacman -S lxappearance-gtk3 --noconfirm --needed
 sudo pacman -S lxrandr --noconfirm --needed
-sudo pacman -S nitrogen --noconfirm --needed
 sudo pacman -S playerctl --noconfirm --needed
 sudo pacman -S rofi --noconfirm --needed
 sudo pacman -S thunar --noconfirm --needed
