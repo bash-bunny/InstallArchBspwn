c6259aeaeeb21241e2862a43dbf6188d18027b81
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-05-21 17:23:18 +0200

    Update configBspwm.sh

--- InstallBswpm/configBspwm.sh
+++ InstallBswpm/configBspwm.sh
@@ -119,6 +119,7 @@ sudo pacman -S --noconfirm --needed recon-ng-git
 sudo pacman -S --noconfirm --needed shellter
 sudo pacman -S --noconfirm --needed theharvester-git
 sudo pacman -S --noconfirm --needed metagoofil
+sudo pacman -S --noconfirm --needed smtp-user-enum
 # For windows
 sudo pacman -S --noconfirm --needed windows-binaries
 sudo pacman -S --noconfirm --needed mimikatz
