fa86a0f5abe2197204a98151c56ae88d83aaefdf
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2020-05-21 17:17:01 +0200

    Update configBspwm.sh

--- InstallBswpm/configBspwm.sh
+++ InstallBswpm/configBspwm.sh
@@ -110,7 +110,7 @@ sudo pacman -S --noconfirm --needed crunch cupp-git cewl
 sudo pacman -S --noconfirm --needed netdiscover
 sudo pacman -S --noconfirm --needed dirsearch
 sudo pacman -S --noconfirm --needed hash-identifier
-sudo pacman -S --noconfirm --needed dnsenum sublist3r-git
+sudo pacman -S --noconfirm --needed dnsenum sublist3r-git dnsrecon
 sudo pacman -S --noconfirm --needed enum4linux
 sudo pacman -S --noconfirm --needed crackmapexec
 sudo pacman -S --noconfirm --needed wafw00f
